152d4a7fcfdc8ef2675f383c65318a7fe5d7d243
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-11-22 10:51:22 +0200

    shaun - pushing globals

--- src/Client/Landing/Landing.js
+++ src/Client/Landing/Landing.js
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import Navbar from "../../Global/Navbar";
+import FollowUs from "../../Global/Header";
 
 const Landing = () => {
   return (
     <View>
-      <Text>Landing</Text>
+      {/* <Text>Landing</Text> */}
+      <FollowUs />
+      <Navbar />
     </View>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;

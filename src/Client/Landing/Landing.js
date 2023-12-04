254ede302b02f47bcb6df75fd979de0391da1d40
mphoChuene <mphochuene42@gmail.com>
2023-12-04 11:03:55 +0200

    landing page

--- src/Client/Landing/Landing.js
+++ src/Client/Landing/Landing.js
@@ -408,7 +408,9 @@ const Landing = ({ navigation }) => {
                 position: "absolute",
                 left: 10,
                 top: "43%",
-                // backgroundColor: "blue",
+                backgroundColor: "#0f0",
+                padding:10,
+                borderRadius:'25px'
               }}>
               <Text style={{ fontSize: 25 }}>{"<"}</Text>
             </TouchableOpacity>

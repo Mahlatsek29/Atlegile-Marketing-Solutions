727cb94881fa96a59fae14d1afd3278e2387a288
Ristar-art <mochochokoboiketlo@gamil.com>
2024-01-11 23:34:17 +0200

    refactor: navigatio to Account and businesses

--- src/Client/AccountAndBusiness/UserProfile.js
+++ src/Client/AccountAndBusiness/UserProfile.js
@@ -346,9 +346,25 @@ const UserProfile = () => {
                 marginRight: "5px",
               }}
               variant="outlined"
+              
               onClick={() => signOut(firebase.auth())}
-            >
+              >
+            
               Sign Out
+            </Button>
+            <Button
+              style={{
+                color: "#072840",
+                borderRadius: "20px",
+                outlineColor: "#072840",
+                marginRight: "5px",
+              }}
+              variant="outlined"
+              onClick={() => {
+                navigation.navigate("AccountAndBusiness");
+              }}
+            >
+              Manage Business
             </Button>
           </View>
         </View>

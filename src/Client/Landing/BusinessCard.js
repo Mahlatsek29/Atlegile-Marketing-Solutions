cc4acd25202270990a750e2d2f525f2aa97857e5
Ristar-art <mochochokoboiketlo@gamil.com>
2024-01-14 20:21:58 +0200

    refactor: banner

--- src/Client/Landing/BusinessCard.js
+++ src/Client/Landing/BusinessCard.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { View, Text, ScrollView, TouchableOpacity, Button } from "react-native";
-import { Grid, Typography } from "@mui/material";
-import { firebase } from "../../config"; // Adjust the path based on your project structure
+import { firebase,firestore, auth } from "../../config"; // Adjust the path based on your project structure
 import ProductCard from "../../Global/Card";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -13,7 +12,8 @@ export default function BusinessCard({ business }) {
   // const scrollViewRef2 = useRef(null);
   // const scrollViewRef3 = useRef(null);
   const [products, setProducts] = useState([]);
-
+  const [banner, setBanner] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
   useEffect(() => {
     const fetchProducts = async () => {
       const productsRef = firebase.firestore().collection("Products");
@@ -53,6 +53,60 @@ export default function BusinessCard({ business }) {
     }
   };
 
+  useEffect(() => {
+    const fetchBanner = async () => {
+      try {
+        const bannerCollection = firestore.collection('Banner');
+        const snapshot = await bannerCollection.get();
+  
+        const bannerData = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            bannerImage: data.bannerImage,
+            discountPrice: data.discountPrice,
+            originalPrice: data.originalPrice,
+            other: data.other,
+            productName: data.productName,
+            quantity: data.quantity,
+          };
+        });
+        console.log("bannerData is ",bannerData)
+        setBanner(bannerData);
+      } catch (error) {
+        console.error('Error fetching banner images:', error);
+      }
+    };
+   
+    fetchBanner();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      
+
+      if (banner[0].bannerImage.length > 0) {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === banner[0].bannerImage.length - 1 ? 0 : prevIndex + 1
+        );
+      }
+    }, 10000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [banner]);
+
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? banner[0].bannerImage.length - 1 : prevIndex - 1
+    );
+  };
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === banner[0].bannerImage.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
     <>
       {business && (
@@ -61,22 +115,25 @@ export default function BusinessCard({ business }) {
             display: "flex",
             flexDirection: "column",
             marginTop: 50,
-            height: "80%",
+            //marginHorizontal:10,
+            alignItems:'center'
+           // height: "60vh",
             // backgroundColor: "red",
           }}>
-          <View>
-            {/* <Text style={{ fontSize: "30px", fontWeight: "bolder" }}>
+          {/* <View>
+            <Text style={{ fontSize: "30px", fontWeight: "bolder" }}>
               {business.businessName}
-            </Text> */}
-          </View>
+            </Text>
+          </View> */}
           <View
             style={{
               width: "80%",
-              marginLeft: "10%",
+             // marginLeft: "10%",
               height: 500,
               justifyContent: "space-between",
               alignItems: "center",
               flexDirection: "row",
+              marginBottom:80
             }}>
             <TouchableOpacity onPress={scrollLeft}>
               <AntDesign name="leftcircle" size={40} color="black" />
@@ -120,7 +177,7 @@ export default function BusinessCard({ business }) {
                 ref={scrollViewRef}
                 horizontal
                 showsHorizontalScrollIndicator={false}
-                style={{ flexDirection: "row" }}
+                style={{ flexDirection: "row"}}
                 onContentSizeChange={(contentWidth) =>
                   handleContentSizeChange(contentWidth)
                 }>
@@ -130,6 +187,82 @@ export default function BusinessCard({ business }) {
               </ScrollView>
             </View>
           </View>
+          {banner.length > 0 ? (
+                    <View
+                      style={{
+                        backgroundImage: `url(${banner[0].bannerImage[currentIndex]})`,
+                        backgroundColor: "gray",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        padding: 15,
+                        flex: 1,
+                        transition: "0.5s ease-in-out",
+                        height:"40vh",
+                        width: "80%",
+
+                      }}
+                    >
+                      <TouchableOpacity
+                        onPress={handlePrevClick}
+                        style={{ marginRight: 20 }}
+                      >
+                        <AntDesign name="left" size={24} color="white" />
+                      </TouchableOpacity>
+                      <View
+                        style={{
+                          flex: 1,
+                          flexDirection: "column",
+                          alignItems: "flex-start",
+                        }}
+                      >
+                        <Text
+                          style={{
+                            fontSize: 15,
+                            fontWeight: 600,
+                            color: "white",
+                          }}
+                        >
+                          {banner[0].other}
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: 25,
+                            fontWeight: 700,
+                            color: "white",
+                          }}
+                        >
+                          {banner[0].productName}
+                        </Text>
+                        <Text>
+                          <Text
+                            style={{
+                              fontSize: 18,
+                              fontWeight: 700,
+                              color: "#c29920",
+                            }}
+                          >
+                            R{banner[0].discountPrice}
+                          </Text>{" "}
+                          <Text
+                            style={{
+                              fontSize: 15,
+                              fontWeight: 400,
+                              color: "white",
+                            }}
+                          >
+                            R{banner[0].originalPrice}
+                          </Text>
+                        </Text>
+                      </View>
+
+                      <TouchableOpacity onPress={handleNextClick}>
+                        <AntDesign name="right" size={24} color="white" />
+                      </TouchableOpacity>
+                    </View>
+                  ) : null}
         </View>
       )}
     </>

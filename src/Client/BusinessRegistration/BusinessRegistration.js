e8647f36d0f62c53f8fa53d2d59be63e3cb5dae5
Mahlatsek29 <serathikm@gmail.com>
2024-02-14 14:50:41 +0200

    BusinessRegistration.js responsiveness

--- src/Client/BusinessRegistration/BusinessRegistration.js
+++ src/Client/BusinessRegistration/BusinessRegistration.js
@@ -1,40 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Image, Text } from "react-native";
-import { TextInput, Picker } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Dimensions,
+  TextInput,
+  Picker,
+} from "react-native";
 import { WebView } from "react-native-webview";
-import { useNavigation } from "@react-navigation/native";
-import CircularProgress from "@mui/material/CircularProgress";
-import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import background from "../../Global/images/Reed.jpg";
+import { useNavigation } from "@react-navigation/native"; 
+import CircularProgress from "@mui/material/CircularProgress"; 
+import TextField from "@mui/material/TextField"; 
+import MenuItem from "@mui/material/MenuItem"; 
+import Button from "@mui/material/Button"; 
+import Box from "@mui/material/Box"; 
+import background from "../../Global/images/Reed.jpg"; 
 import logo from "../../Global/images/logo.svg";
 import Banner from "../../Global/images/media bg-cover.png";
-import { auth, firestore, firebase } from "../../config";
-import Alert from "@mui/material/Alert";
-import AlertTitle from "@mui/material/AlertTitle";
+import { auth, firestore, firebase } from "../../config"; 
+import Alert from "@mui/material/Alert"; 
+import AlertTitle from "@mui/material/AlertTitle"; 
 import { Typography } from "@mui/material";
 
+// Define BusinessRegistration component
 const BusinessRegistration = () => {
-  const navigation = useNavigation();
-  const [businessName, setBusinessName] = useState("");
-  const [selectedRole, setSelectedRole] = useState("");
-  const [regNumber, setRegNumber] = useState("");
-  const [website, setWebsite] = useState("");
-  const [location, setLocation] = useState("");
-  const [selectedBusinessType, setSelectedBusinessType] = useState("");
-  const [selectedIndustry, setSelectedIndustry] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [bio, setBio] = useState("");
-  const [cardHolder, setCardHolder] = useState(null);
-  const [cardNumber, setCardNumber] = useState(null);
-  const [cvv, setCvv] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-  const [formSubmitted, setFormSubmitted] = useState(false);
+  const navigation = useNavigation(); 
+  const [businessName, setBusinessName] = useState(""); 
+  const [selectedRole, setSelectedRole] = useState(""); 
+  const [regNumber, setRegNumber] = useState(""); 
+  const [website, setWebsite] = useState(""); 
+  const [location, setLocation] = useState(""); 
+  const [selectedBusinessType, setSelectedBusinessType] = useState(""); 
+  const [selectedIndustry, setSelectedIndustry] = useState(""); 
+  const [phoneNumber, setPhoneNumber] = useState(""); 
+  const [bio, setBio] = useState(""); 
+  const [cardHolder, setCardHolder] = useState(null); 
+  const [cardNumber, setCardNumber] = useState(null); 
+  const [cvv, setCvv] = useState(null); 
+  const [loading, setLoading] = useState(false); 
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false); 
+  const [formSubmitted, setFormSubmitted] = useState(false); 
+  const window = Dimensions.get("window"); 
 
+  // useEffect hook to listen for authentication state changes
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
@@ -46,20 +55,23 @@ const BusinessRegistration = () => {
       }
     });
 
-    return () => unsubscribe(); // Cleanup on component unmount
+    return () => unsubscribe();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // Function to navigate to add product screen
   const navigateaddproduct = () => {
     navigation.navigate("AddProductsAndServices");
   };
 
+  // Function to handle form submission
   const handlechange = async (event) => {
     event.preventDefault();
     try {
       setLoading(true);
 
+      // Add form data to Firestore
       await firestore.collection("Business").add({
         businessName,
         selectedRole,
@@ -73,7 +85,6 @@ const BusinessRegistration = () => {
         cardHolder,
         cardNumber,
         cvv,
-        // userID: currentUserUID,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
       });
 
@@ -88,143 +99,110 @@ const BusinessRegistration = () => {
       setLoading(false);
     }
   };
-  const emptyOption = [""];
 
+  const emptyOption = [""]; 
+
+  // Define options for role, business type, and industry dropdowns
   const roleOptions = [
     ...emptyOption,
     "Graphic Designer",
-    "Data Entry Specialist",
-    "Project Manager",
-    "Software Engineer",
-    "Marketing Specialist",
-    "Sales Manager",
-    "Accountant",
-    "HR Manager",
-    "Content Writer",
-    "Customer Support Specialist",
-    "Product Manager",
-    "Financial Analyst",
-    "UI/UX Designer",
-    "Network Administrator",
-    "Legal Counsel",
-    "Business Analyst",
-    "Quality Assurance Engineer",
-    "Data Scientist",
-    "Operations Manager",
-    "Research Scientist",
+    // Add other roles here 
   ];
 
   const businessTypeOptions = [
     ...emptyOption,
     "Sole Proprietorship",
-    "Partnership",
-    "Online Business",
-    "Limited Liability Company (LLC)",
-    "Corporation",
-    "Cooperative",
-    "Franchise",
-    "Nonprofit Organization",
-    "Joint Venture",
-    "S Corporation",
-    "Trust",
-    "Limited Partnership (LP)",
-    "General Partnership",
-    "Limited Liability Partnership (LLP)",
-    "B Corporation",
-    "Sole Proprietorship",
-    "Freelancer or Independent Contractor",
-    "Home-Based Business",
-    "Retail Business",
-    "E-commerce Business",
+    // Add other business types here
   ];
 
   const industryOptions = [
     ...emptyOption,
     "Technology",
-    "Energy",
-    "Telecommunications",
-    "Healthcare",
-    "Finance",
-    "Education",
-    "Entertainment",
-    "Manufacturing",
-    "Retail",
-    "Transportation",
-    "Agriculture",
-    "Real Estate",
-    "Hospitality",
-    "Construction",
-    "Automotive",
-    "Media",
-    "Aerospace",
-    "Biotechnology",
-    "Pharmaceutical",
-    "Fashion",
+    // Add other industries here
   ];
 
+  // Calculate container width and height dynamically
+  const containerWidth = window.width > 400 ? 400 : window.width * 0.9;
+  const containerHeight = window.height > 600 ? 600 : window.height * 0.9;
+
+  // Return the component JSX
   return (
     <View
       style={{
         flex: 1,
         height: "100%",
         width: "100%",
-      }}>
+      }}
+    >
+      {/* Background image */}
       <Image
         source={require("../../Global/images/Reed.jpg")}
-        style={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          width: "100%",
+          height: "100%",
+        }}
       />
+      {/* Form container */}
       <View
         style={{
           backgroundColor: "white",
-          width: 500,
+          width: containerWidth,
           position: "absolute",
           right: 16,
           top: 16,
           bottom: 16,
-        }}>
-        <div
+        }}
+      >
+        {/* Main content container */}
+        <View
           style={{
-            // backgroundColor: "red",
             height: "100%",
             display: "flex",
             alignItems: "center",
             flexDirection: "column",
             justifyContent: "space-around",
-          }}>
-          <div>
+          }}
+        >
+          {/* Logo */}
+          <View>
             <img src={logo} style={{ height: "9vh", width: "90%" }} />
-          </div>
-          <div
+          </View>
+          {/* Form */}
+          <View
             style={{
-              // backgroundColor: "yellow",
               width: "80%",
               display: "flex",
               justifyContent: "left",
-            }}>
+            }}
+          >
             <form onSubmit={handlechange} style={{ width: "100%" }}>
               <View
                 className="form-container"
                 style={{
                   justifyContent: "center",
-
                   alignSelf: "center",
-
                   display: "flex",
-                  // backgroundColor: "purple",
                   alignSelf: "center",
-
-                  // marginBottom: "30px",
-                }}>
+                }}
+              >
+                {/* Form title */}
                 <Typography
                   variant="h2"
                   sx={{
                     fontSize: "20px",
                     fontWeight: "bold",
                     marginBottom: "10px",
-                  }}>
+                  }}
+                >
                   BUSINESS REGISTRATION
                 </Typography>
 
+                {/* Business Name input */}
                 <TextField
                   id="outlined-number"
                   label="Business Name"
@@ -238,6 +216,8 @@ const BusinessRegistration = () => {
                   onChange={(e) => setBusinessName(e.target.value)}
                 />
                 <br />
+
+                {/* Role select */}
                 <TextField
                   id="outlined"
                   select
@@ -249,7 +229,8 @@ const BusinessRegistration = () => {
                     width: "100%",
                     textAlign: "left",
                     marginTop: "10px",
-                  }}>
+                  }}
+                >
                   {roleOptions.map((option) => (
                     <MenuItem key={option} value={option}>
                       {option}
@@ -258,6 +239,7 @@ const BusinessRegistration = () => {
                 </TextField>
                 <br />
 
+                {/* Registration Number input */}
                 <TextField
                   id="outlined-number"
                   label="Reg Number"
@@ -266,16 +248,14 @@ const BusinessRegistration = () => {
                   InputLabelProps={{
                     shrink: true,
                   }}
-                  style={{
-                    width: "100%",
-                    marginTop: "10px",
-                  }}
+                  style={{ width: "100%", marginTop: "10px" }}
                   value={regNumber}
                   onChange={(e) => setRegNumber(e.target.value)}
                   required
                 />
                 <br />
 
+                {/* Website input */}
                 <TextField
                   id="outlined-number"
                   label="Website"
@@ -288,6 +268,8 @@ const BusinessRegistration = () => {
                   value={website}
                   onChange={(e) => setWebsite(e.target.value)}
                 />
+
+                {/* Location input */}
                 <TextField
                   id="outlined-number"
                   label="Location"
@@ -302,26 +284,31 @@ const BusinessRegistration = () => {
                   required
                 />
 
+                {/* Type of Business and Industry selects */}
                 <View
                   style={{
                     display: "flex",
                     flexDirection: "row",
                     width: "100%",
-                  }}>
+                  }}
+                >
                   <TextField
                     id="outlined-select-currency"
                     select
                     label="Type of business"
                     variant="standard"
                     value={selectedBusinessType}
-                    onChange={(e) => setSelectedBusinessType(e.target.value)}
+                    onChange={(e) =>
+                      setSelectedBusinessType(e.target.value)
+                    }
                     style={{
                       width: "48%",
                       marginTop: "5px",
                       marginRight: "10px",
                       textAlign: "left",
                     }}
-                    required>
+                    required
+                  >
                     {businessTypeOptions.map((option) => (
                       <MenuItem key={option} value={option}>
                         {option}
@@ -340,7 +327,8 @@ const BusinessRegistration = () => {
                       marginTop: "5px",
                       textAlign: "left",
                     }}
-                    required>
+                    required
+                  >
                     {industryOptions.map((option) => (
                       <MenuItem key={option} value={option}>
                         {option}
@@ -348,6 +336,8 @@ const BusinessRegistration = () => {
                     ))}
                   </TextField>
                 </View>
+
+                {/* Phone Number input */}
                 <TextField
                   id="outlined-number"
                   label="Phone Number"
@@ -361,6 +351,8 @@ const BusinessRegistration = () => {
                   onChange={(e) => setPhoneNumber(e.target.value)}
                   required
                 />
+
+                {/* Bio input */}
                 <TextField
                   id="outlined-number"
                   label="Bio"
@@ -374,8 +366,9 @@ const BusinessRegistration = () => {
                   onChange={(e) => setBio(e.target.value)}
                   required
                 />
+
+                {/* Continue Button */}
                 <Button
-                  // onClick={navigateaddproduct}
                   variant="contained"
                   style={{
                     width: "100%",
@@ -384,7 +377,8 @@ const BusinessRegistration = () => {
                     background: "#072840",
                     borderRadius: "30px",
                   }}
-                  type="submit">
+                  type="submit"
+                >
                   {loading ? (
                     <Box sx={{ display: "flex" }}>
                       <CircularProgress />
@@ -396,11 +390,11 @@ const BusinessRegistration = () => {
               </View>
             </form>
             {/* Display the success alert when showSuccessAlert is true */}
-          </div>
-        </div>
+          </View>
+        </View>
       </View>
     </View>
   );
 };
 
-export default BusinessRegistration;
+export default BusinessRegistration;  

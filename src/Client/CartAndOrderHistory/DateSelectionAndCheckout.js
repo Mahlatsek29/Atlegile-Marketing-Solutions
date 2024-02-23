58f34392c5f2e8716b527eddff4d610a5f8adee5
Ristar-art <mochochokoboiketlo@gamil.com>
2024-02-24 01:03:25 +0200

    feat :added a marker

--- src/Client/CartAndOrderHistory/DateSelectionAndCheckout.js
+++ src/Client/CartAndOrderHistory/DateSelectionAndCheckout.js
@@ -44,6 +44,8 @@ import PlacesAutocomplete, {
 } from "react-places-autocomplete";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import { Asset } from "expo-asset";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
@@ -88,6 +90,16 @@ const DateSelectionAndCheckout = () => {
   const [editCode, setEditCode] = useState(false);
   const [editCounty, setEditCountry] = useState(false);
   const [authPin,setAuthPIn] = useState('')
+
+  const icon = require('../../../assets/marker.png');
+  const iconURI = Asset.fromModule(icon).uri;
+
+  const leafletIcon = new L.Icon({
+    iconUrl: iconURI,
+    iconSize: [30, 30],
+    // iconAnchor: [22, 94],
+    // popupAnchor: [-3, -76],
+  });
   // useEffect hook to listen for changes in authentication state
   useEffect(() => {
     // Get the authentication instance
@@ -1420,7 +1432,7 @@ const DateSelectionAndCheckout = () => {
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                           />
-                          <Marker position={[coordinates.lat, coordinates.lng]}>
+                          <Marker position={[coordinates.lat, coordinates.lng]}  icon={leafletIcon}>
                             <Popup>
                               <FontAwesomeIcon
                                 icon={faMapMarkerAlt}
